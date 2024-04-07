4701ab0285909f15323d4baccc69b842cdef5463
Ushan Ikshana <89129852+im-ushan-ikshana@users.noreply.github.com>
2024-04-08 00:42:18 +0530

    new_file

--- ds_code.py
+++ ds_code.py
@@ -11,16 +11,7 @@ def get_page(url):
 def convert_to_soup(html):
     return BeautifulSoup(html, 'lxml')
 
-#find all classes
-def findAllClasses(soup):
-    return soup.find_all(class_=True)
-
-def findAllIds(soup):
-    return soup.find_all(id=True)
-
-def findAllTags(soup):
-    return soup.find_all(True)
-
+# Find functions - these functions will be used to find elements in the soup object
 def findGivenTag(soup, tag):
     return soup.find_all(tag)
 
@@ -39,16 +30,56 @@ def findGivenTagAndId(soup, tag, id_name):
 def findGivenClassAndId(soup, class_name, id_name):
     return soup.find_all(class_=class_name, id=id_name)
 
+#convert soup to readable text
+def convertSoupToText(soup):
+    return soup.get_text()
+
+#get Lecturer name
+def getLecturerName(soup):
+    if soup:
+        lecturer_name = soup.find('h3', class_='sppb-addon-title')
+        if lecturer_name:
+            return lecturer_name.get_text()
+    else:
+        return None
+
+#get Lecturer - designation
+def getLecturerDesignation(soup):
+    if soup:
+        lecturer_designation = soup.find('strong')
+        if lecturer_designation:
+            return lecturer_designation.get_text() if lecturer_designation else ""
+    else:
+        return None
+
+# Print function - this function will be used to print the output   
+def myPrinter(String_to_print):
+    if String_to_print is not None and String_to_print.strip() != "":
+        print(String_to_print)
 
 
 def main():
+    print("-------------------Start-------------------")
     url =  "https://mit.kln.ac.lk/index.php/staff/academic-staff"
     htlm_file , response_code = get_page(url)
-    
     if(response_code):
         soup = convert_to_soup(htlm_file)
-        
+        sectionSoup = findGivenClass(soup, 'sppb-section')
+        for Section in sectionSoup:
+            colomunSection = findGivenClass(Section, 'sppb-row')
+            for innerSection in colomunSection:
+                myPrinter(getLecturerName(innerSection))
+                detailSection = findGivenClass(innerSection, 'sppb-addon-content')
+                for innerInnerSection in detailSection:
+                    myPrinter(getLecturerDesignation(innerInnerSection))
+                    print(innerInnerSection)
+                    print("\n")
+                        
 
+                
+
+
+        
 
 if __name__ == "__main__":
     main()
